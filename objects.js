6bf3cde48a54930443fa8f1fefebfbf95a849695
Jennifer E <je@jenniferevenson.com>
2018-01-11 04:51:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { "Ed Sheeran": "Shape of You", 'Soha': 'Mil Pasos', 'Luis Fonsi': 'Despacito', 'Alberto Podesta': 'La Capilla Blanca', "Dexy's Midnight Runners": 'Come on Eileen' };
+var playlist = { 'Soha': 'Mil Pasos'};
 
 function updatePlaylist(playlist, artist, song) {
   // non destructively add artist:song to object "playlist"
@@ -12,12 +12,15 @@ function removeFromPlaylist(playlist, artist) {
 }
 
 // reference parking lot 
+
 //non destructively add key-value pair to object 
 /*
 return Object.assign({}, obj, { [key]: value })
-*/
 
-// destructively add key-value pair to object 
-/*
 return Object.assign(obj, { [key]: value })
 */
+// destructively add key-value pair to object 
+
+
+// var playlist = { "Ed Sheeran": "Shape of You", 'Soha': 'Mil Pasos', 'Luis Fonsi': 'Despacito', 'Alberto Podesta': 'La Capilla Blanca', "Dexy's Midnight Runners": 'Come on Eileen' };
+
