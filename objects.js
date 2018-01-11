f747ea91b26b443fe921f27f64b284d2ed9cbc53
Jennifer E <je@jenniferevenson.com>
2018-01-11 04:29:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,2 @@
-var playlist = { "Ed Sheeran": "Shape of You", 'Soha': 'Mil Pasos', 'Luis Fonsi': 'Despacito',  };
+var playlist = { "Ed Sheeran": "Shape of You", 'Soha': 'Mil Pasos', 'Luis Fonsi': 'Despacito', 'Alberto Podesta': 'La Capilla Blanca', 'Dexy"s Midnight Runners': 'Come on Eileen' };
 
